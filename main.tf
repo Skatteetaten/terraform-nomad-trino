8b3e45bbea2e093ab04d97c0ec869b1e1c578ae9
Fredrik Hoem Grelland <fredrikhoem.grelland@skatteetaten.no>
2020-09-10 17:53:47 +0200

    Add consul connect native cluster option to module #12

--- main.tf
+++ main.tf
@@ -5,20 +5,32 @@ locals {
       "JUST_EXAMPLE_ENV=some-value",
     ], var.container_environment_variables)
   )
+
+  template_standalone = file("${path.module}/conf/nomad/presto_standalone.hcl")
+  template_cluster    = file("${path.module}/conf/nomad/presto.hcl")
+
+  node_types = var.coordinator ? ["coordinator", "worker"] : ["worker"]
 }
 
+
 data "template_file" "template-nomad-job-presto" {
-  template = file("${path.module}/conf/nomad/presto.hcl")
+  template = var.mode == "standalone" ? local.template_standalone : local.template_cluster
 
   vars = {
     nomad_job_name = var.nomad_job_name
     datacenters    = local.datacenters
     namespace      = var.nomad_namespace
 
-    service_name = var.service_name
-    port         = var.port
-    image        = var.docker_image
-    envs         = local.presto_env_vars
+    service_name          = var.service_name
+    node_types            = jsonencode(local.node_types)
+    local_docker_image    = var.local_docker_image
+    workers               = var.workers
+    cluster_shared_secret = var.cluster_shared_secret
+    docker_image          = var.docker_image
+    envs                  = local.presto_env_vars
+    debug                 = var.debug
+    memory                = var.memory
+    consul_http_addr      = var.consul_http_addr
 
     #hivemetastore
     hivemetastore_service_name = var.hivemetastore.service_name
--- variables.tf
+++ variables.tf
@@ -20,24 +20,73 @@ variable "nomad_job_name" {
   default     = "presto"
 }
 
+variable "local_docker_image" {
+  type        = bool
+  description = "Switch for nomad jobs to use artifact for image lookup"
+  default     = false
+}
+
+variable "mode" {
+  type        = string
+  description = "Switch for nomad jobs to use cluster or standalone deployment"
+  default     = "standalone"
+  validation {
+    condition     = var.mode == "standalone" || var.mode == "cluster"
+    error_message = "Valid modes: \"cluster\" or \"standalone\"."
+  }
+}
+
 # presto
+variable "cluster_shared_secret" {
+  type        = string
+  description = "Shared secret between coordinator and workers"
+}
+
 variable "service_name" {
   type        = string
   description = "Presto service name"
   default     = "presto"
 }
 
-variable "port" {
+variable "coordinator" {
+  type        = bool
+  description = "Include a coordinator in addition to the workers. Set this to `false` when extending an existing cluster"
+  default     = true
+}
+
+variable "workers" {
   type        = number
-  description = "Presto http port"
-  default     = 8080
+  description = "cluster: Number of nomad worker nodes"
+  default     = 1
+}
+
+variable "debug" {
+  type        = bool
+  description = "Turn on debug logging in presto nodes"
+  default     = false
+}
+
+variable "consul_http_addr" {
+  type        = string
+  description = "Address to consul, resolvable from the container. e.g. http://127.0.0.1:8500"
+}
+
+variable "memory" {
+  type        = number
+  description = "Memory allocation for presto nodes"
+  default     = 1024
+  validation {
+    condition = var.memory >= 738
+    error_message = "Presto can not run with less than 512MB of memory. 256MB is subtracted for OS. Total must be at least 738MB."
+  }
 }
 
 variable "docker_image" {
   type        = string
   description = "Presto docker image"
-  default     = "prestosql/presto:333"
+  default     = "prestosql/presto:341"
 }
+
 variable "container_environment_variables" {
   type        = list(string)
   description = "Presto environment variables"
