a5b74176dbe99fbfcd9c169c751ce7c36943facb
k16592 <danielgerhardsen.nilsen@skatteetaten.no>
2020-10-12 13:33:54 +0200

    bump version hive and fixed postgres variable names

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,7 +17,9 @@
 - Use docker instead fo binaries #21
 - size vagrant box to runner in github actions #23
 - Sync origin template #28
-- Bumped version for minio, hive and postgres #34
+- Bumped version minio: 0.0.2 -> 0.1.0
+- Bumped version hive: 0.0.2 -> 0.1.0
+- Bumped version postgres: 0.0.1 -> 0.1.0
 - Consul Connect plugin optional with configurable artifact source #30
 
 ## Fixed
--- example/main.tf
+++ example/main.tf
@@ -84,13 +84,13 @@ module "postgres" {
   nomad_namespace   = local.nomad_namespace
 
   # postgres
-  postgres_service_name                    = "postgres"
-  postgres_container_image                 = "postgres:12-alpine"
-  postgres_container_port                  = 5432
-  postgres_admin_user                      = "hive"
-  postgres_admin_password                  = "hive"
-  postgres_database                        = "metastore"
-  postgres_container_environment_variables = ["PGDATA=/var/lib/postgresql/data"]
+  service_name                    = "postgres"
+  container_image                 = "postgres:12-alpine"
+  container_port                  = 5432
+  admin_user                      = "hive"
+  admin_password                  = "hive"
+  database                        = "metastore"
+  container_environment_variables = ["PGDATA=/var/lib/postgresql/data"]
 }
 
 module "hive" {
