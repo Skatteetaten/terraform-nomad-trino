0f3400805eecaff33687af88e69e17d9cb3dbc61
Claes-Jonas Gill <Claes-Jonas.Gill@skatteetaten.no>
2021-04-20 12:37:25 +0200

    Upd nomad provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     nomad = {
       source  = "hashicorp/nomad"
-      version = "~> 1.4.14"
+      version = "1.4.14"
     }
     template = {
       source  = "hashicorp/template"
